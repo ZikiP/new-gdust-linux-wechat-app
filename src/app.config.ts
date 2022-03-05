6b1f85d1df0aa98710970cc65bee228427fbee52
Ziki <2019542792@qq.com>
2022-03-05 10:57:56 +0800

    feat: 添加tabbar配置

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,11 +1,28 @@
 export default {
   pages: [
-    'pages/index/index'
+    'pages/index/index',
+    'pages/user/index'
   ],
   window: {
     backgroundTextStyle: 'light',
-    navigationBarBackgroundColor: '#fff',
-    navigationBarTitleText: 'WeChat',
-    navigationBarTextStyle: 'black'
+    navigationBarTitleText: 'e广科',
+    navigationBarTextStyle: 'white',
+  },
+  tabBar: {
+    color: "#8a8a8a",
+    selectedColor: "#1296db",
+    backgroundColor: '#FFF',
+    borderStyle: "white",
+    list: [{
+      pagePath: 'pages/index/index',
+      text: '首页',
+      iconPath: 'assets/images/index/index.png',
+      selectedIconPath: 'assets/images/index/index@active.png'
+    }, {
+      pagePath: 'pages/user/index',
+      text: '个人',
+      iconPath: 'assets/images/index/more.png',
+      selectedIconPath: 'assets/images/index/more@active.png'
+    }],
   }
 }
