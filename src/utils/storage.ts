a7e3bb543c98bd3232ab16fec9f86fa66dc3e04a
Ziki <2019542792@qq.com>
2022-03-06 14:05:01 +0800

    feat: 增加storage处理函数

---
+++ src/utils/storage.ts
@@ -0,0 +1,33 @@
+import Taro from "@tarojs/taro";
+
+export const setCache = (key: string, value: string | number) => {
+  if(!key || !value) {
+    return;
+  }
+  try {
+    Taro.setStorageSync(key, value)
+  } catch (error) {
+    console.log(error)
+  }
+}
+export const getCache = (key: string) => {
+  if(!key) {
+    return ;
+  }
+  try {
+    const value =  Taro.getStorageSync(key)
+    return value
+  } catch (error) {
+    console.log(error)
+  }
+}
+export const removeCache = (key: string) => {
+   if(!key) {
+     return;
+   }
+   try {
+     Taro.removeStorageSync(key)
+   } catch (error) {
+    console.log(error)
+   }
+}
