7843e32252d1b23afd64494a869b8bafd56c1541
Ziki <2019542792@qq.com>
2022-03-06 14:02:52 +0800

    feat: 增加功能卡片组件

---
+++ src/components/CCard/index.tsx
@@ -0,0 +1,16 @@
+import { View } from '@tarojs/components'
+
+
+type PropsType = {
+  content: any
+}
+
+export const CCard = (props: PropsType): JSX.Element => {
+  return (
+    <View className='bg-white border-2 border-gray-200 rounded-md shadow-2xl bg-blend-screen p-8 rounded-xl'>
+      <View className='p-4'>
+        {props.content}
+      </View>
+    </View>
+  )
+}
