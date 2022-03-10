b03f214a2035e7ccd323c08b2eb2dd725bf37c0c
Ziki <2019542792@qq.com>
2022-03-10 14:32:58 +0800

    fix: 修改组件样式

--- src/components/Notice/index.tsx
+++ src/components/Notice/index.tsx
@@ -11,7 +11,7 @@ export const Notice = (props: PropsType): JSX.Element => {
     <View className='bg-white  shadow-2xl bg-blend-screen py-8 pr-2 rounded-xl'>
       <View className='py-4 pr-2'>
         <View className='h-20 flex justify-between border-0 border-l-8 border-solid border-blue-400'>
-          <Text className='text-xs notice-color line'>{props.title}</Text>
+          <Text className='text-xs notice-color line ml-9'>{props.title}</Text>
           <Text className='text-14 notice-color mr-8'>{'MORE >'}</Text>
         </View>
         <View className='text-14 px-16 mt-16 text-gray-600'>
