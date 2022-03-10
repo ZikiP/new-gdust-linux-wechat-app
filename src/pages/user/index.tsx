6e778e870ad48d36a4bef886f43aa6b13ec956fa
Ziki <2019542792@qq.com>
2022-03-10 14:29:52 +0800

    feat: 完成个人页面内容

--- src/pages/user/index.tsx
+++ src/pages/user/index.tsx
@@ -1,10 +1,97 @@
-import React from 'react'
+import { OpenData, View, Navigator,Text } from '@tarojs/components'
+import { useDidShow, useReady } from '@tarojs/taro'
+import { useState } from 'react'
+import { loginLoad } from '~/src/service/handleService'
+import { getUserInfo } from '~/src/service/servers'
+import { getCache } from '~/src/utils/storage'
+import UCard from '@/components/UCard'
+import Issue from '@img/more/issue.png'
+import About from '@img/more/aboutmiao.png'
+import './index.scss'
+import Taro from '@tarojs/taro'
 
-const User = (props): JSX.Element =>  {
+type infoType = {
+  class_name: string
+  department_name: string
+  name: string
+  student_number: string
+}
+
+const cardList =[
+  {
+    icon: Issue,
+    title: '意见反馈',
+    url:''
+  },
+  {
+    icon: About,
+    title: '关于e广科',
+    url: '/pages/more/about'
+  }
+]
+
+const User = (): JSX.Element =>  {
+  const[userInfo, setUserInfo] = useState<infoType>(Object)
+  useReady(() => {
+    loginLoad().then(()=> {
+      getData();
+    })
+  })
+  const getData = async() => {
+    const account = getCache('account')
+    const userInfoRes: any = await getUserInfo(account)
+    setUserInfo(userInfoRes.detail)
+  }
+
+  const goView = (url: string) => {
+      Taro.navigateTo({
+        url
+      })
+  }
+
   return (
-    <div>
+    <View>
+      <View className='theme-color h-80 '></View>
+       <View className='bg-white w-95_100 h-135 border-solid border-gray-100 border-1 mx-auto shadow-2xl rounded-xl relative -top-30'>
+          <View className='flex justify-center'>
+            <View >
+              <View className='w-60 h-60 border-solid border border-gray-100 circle overflow-hidden bg-dark-200 relative -top-30' ><OpenData type='userAvatarUrl'/></View>
+              <View className='username-color w-60 text-center relative -top-20'>{userInfo?.name}</View>
+            </View>
+          </View>
+        <View className='grid grid-cols-3 gap-x-4 py-8 text-center relative -top-15 text-gray-600 text-14'>
+          <View>
+            <View className='mb-5'>学号</View>
+            <View>{userInfo?.student_number}</View>
+          </View>
+          <View>
+            <View className='mb-5'>系别</View>
+            <View>{userInfo?.department_name}</View>
+          </View>
+          <View>
+            <View className='mb-5'>班级</View>
+            <View>{userInfo?.class_name}</View>
+          </View>
+        </View>
+      </View>
+      <View className='py-0 shadow-2xl w-95_100 mx-auto relative -top-15 text-gray-600 text-14'>
+      {
+        cardList &&
+        cardList.map((item, index) => {
+          return (
+          <View key={index} className='flex flex-col items-left' onClick={() => goView(item.url)}>
+            <UCard icon={item.icon} title={item.title} url={item.url}></UCard>
+          </View>
+          )
+        }
+        )
+      }
+      </View>
+      <Navigator url='../more/login' className='text-18 leading-36 h-36 bg-red-400 w-95_100 items-center text-white rounded-xl mt-0 mx-9 mb-5 absolute bottom-0'>
+        <Text className='flex justify-center'>切换绑定</Text>
+      </Navigator>
+    </View>
 
-    </div>
   )
 }
 
