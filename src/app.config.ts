2d5392f40edc1007e586d4e47c30e04b8864b360
Ziki <2019542792@qq.com>
2022-03-15 17:46:34 +0800

    feat: 添加全局配置

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,12 +1,20 @@
 export default {
   pages: [
     'pages/index/index',
-    'pages/user/index'
+    'pages/user/index',
+    'pages/more/login',
+    'pages/more/about',
+  ],
+  subPackages: [
+    { root:'subPackages/classSchedule', pages:['index']}
   ],
   window: {
     backgroundTextStyle: 'light',
     navigationBarTitleText: 'e广科',
     navigationBarTextStyle: 'white',
+    navigationBarBackgroundColor: '#079df2',
+    // 开启下拉刷新
+    enablePullDownRefresh: true
   },
   tabBar: {
     color: "#8a8a8a",
@@ -24,5 +32,7 @@ export default {
       iconPath: 'assets/images/index/more.png',
       selectedIconPath: 'assets/images/index/more@active.png'
     }],
-  }
+  },
+  // lazyCodeLoading: "requiredComponents" // 按需注入代码
+
 }
