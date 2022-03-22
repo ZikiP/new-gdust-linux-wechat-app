a43e02209a0fb39ecfbb1101c83ed4099f008c95
Ziki <2019542792@qq.com>
2022-03-22 21:01:09 +0800

    docs: 完善注释

--- src/utils/Debounce.ts
+++ src/utils/Debounce.ts
@@ -9,14 +9,17 @@ type currentType = {
  * 防抖hooks
  * @param fn 需要防抖的函数
  * @param delay 间隔
- * @param dep
+ * @param dep 空数组，无需输入任何东西
  * @returns
  */
-export function useDebounce(fn, delay, dep = []) {
+export function useDebounce(fn: Function, delay: number, dep:Array<any> = []) {
+
   const { current } = useRef<currentType>({ fn, timer: null });
+
   useEffect(function () {
     current.fn = fn;
   }, [fn]);
+
   return useCallback(function f(...args) {
     if (current.timer) {
       clearTimeout(current.timer);
@@ -25,4 +28,5 @@ export function useDebounce(fn, delay, dep = []) {
       current.fn.call(this, ...args);
     }, delay);
   }, dep)
+
 }
