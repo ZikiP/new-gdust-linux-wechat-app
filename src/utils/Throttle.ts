5d77383013b4b5f2eddac2d8e083871db12cbc4c
Ziki <2019542792@qq.com>
2022-03-25 13:17:16 +0800

    docs: 完善注释

--- src/utils/Throttle.ts
+++ src/utils/Throttle.ts
@@ -5,11 +5,12 @@ import { useCallback, useEffect, useRef } from "react"
  * 节流hooks
  * @param fn 需要节流的函数
  * @param delay 间隔
- * @param dep
+ * @param dep 空数组，无需传入参数
  * @returns
  */
 export function useThrottle(fn, delay, dep = []) {
   const { current } = useRef<any>({ fn, timer: null })
+
   useEffect(
     function () {
       current.fn = fn
