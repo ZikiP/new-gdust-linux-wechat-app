b07bf68c685856ee4ffe7e02e42cb20d4954e929
Ziki <2019542792@qq.com>
2022-04-05 15:54:17 +0800

    feat: 添加日志卡片组件

--- src/pages/more/About/index.tsx
+++ src/pages/more/About/index.tsx
@@ -2,10 +2,63 @@ import { View, Text, Image } from "@tarojs/components"
 import { useState } from "react"
 import Gx from '@img/more/gx.png'
 import { useReady } from "@tarojs/taro"
+import {LogCard} from '@/components/LogCard'
+
+type aboutItem = {
+  version: string
+  date: string
+  content: Array<object>
+}
+
+type aboutType = Array<aboutItem>
 
 const About = (): JSX.Element => {
   const [isShow, setIsShow] = useState(false)
   const [year, setYear] = useState<number>()
+
+  const aboutList: aboutType = [
+    {
+      version: '1.0.1',
+      date: '2022.04.05',
+      content: [
+        {data: '对部分样式进行修改'},
+        {data: '修改绑定功能密码输入框输入条件'},
+        {data: '添加日志组件卡片'},
+        {data: '添加请求出错时的提示'}
+      ]
+    },
+    {
+      version: '1.0.0',
+      date: '2022.04.03',
+      content: [
+        {data: '全用全新技术栈进行重构'},
+      ]
+    },
+    {
+      version: '0.3.0',
+      date: '2022.02.14',
+      content: [
+        {data: '新增成绩查询'},
+        {data: '新增系统公告'},
+        {data: '修复刷新异常'}
+      ]
+    },
+    {
+      version: '0.2.1',
+      date: '2022.02.15',
+      content: [
+        {data: '修复首页错位问题'}
+      ]
+    },
+    {
+      version: '0.2.0 公测版',
+      date: '2022.02.16',
+      content: [
+        {data: '提交审核版本'}
+      ]
+    },
+
+  ]
 
   useReady(() => {
     setYear(new Date().getFullYear())
@@ -16,8 +69,8 @@ const About = (): JSX.Element => {
       <View className="bg-white h-50 flex items-center px-22 box-border border-0 border-b-1 border-solid border-b-gray-200 ">
         <View className="flex flex-1 items-center">
           <Text className="text-gray-600 pr-5 text-18">e广科</Text>
-          <Text className="theme-color mt-3 border-1 border-solid text-14 theme-border-color leading-17 px-4 mr-5">v0.3.0</Text>
-          <Text className="theme-color mt-3 border-1 border-solid text-14 theme-border-color leading-17 px-4">正式版</Text>
+          <Text className="theme-color mt-3 border-1 border-solid text-14 theme-border-color leading-17 px-4 mr-5">v1.0.1</Text>
+          <Text className="theme-color mt-3 border-1 border-solid text-14 theme-border-color leading-17 px-4">体验版</Text>
         </View>
         <View className="theme-color">
           {isShow ? <Text  onClick={() => setIsShow(!isShow)}>简介</Text> : <Text onClick={() => setIsShow(!isShow)}>更新日志</Text>}
@@ -27,38 +80,21 @@ const About = (): JSX.Element => {
         {
           isShow ?
             <View className="flex flex-col items-stretch">
-              <View className="flex flex-col leading-28 mt-7 border-0 border-solid border-t-1 border-b-1 border-gray-200 bg-white flex-nowrap box-border pt-10 px-22 pb-15 w-full text-gray-600">
-                <View className="flex justify-between text-18  mb-8 leading-32">
-                  <Text className="flex-1">v0.3.0</Text>
-                  <Text className="text-16 text-gray-400">2022.02.16</Text>
-                </View>
-                <Text className="pb-8 text-16">1. 新增成绩查询</Text>
-                <Text className="pb-8 text-16">2. 新增系统公告</Text>
-                <Text className="pb-8 text-16">3. 修复刷新异常</Text>
-              </View>
-              <View className="flex flex-col leading-28 mt-7 border-0 border-solid border-t-1 border-b-1 border-gray-200 bg-white flex-nowrap box-border pt-10 px-22 pb-15 w-full text-gray-600">
-                <View className="flex justify-between text-18  mb-8 leading-32">
-                  <Text className="flex-1">v0.2.1 </Text>
-                  <Text className="text-16 text-gray-400">2022.02.15</Text>
-                </View>
-                <Text className="pb-8 text-16">1. 修复首页错位问题</Text>
-              </View>
-              <View className="flex flex-col leading-28 mt-7 border-0 border-solid border-t-1 border-b-1 border-gray-200 bg-white flex-nowrap box-border pt-10 px-22 pb-15 w-full text-gray-600">
-                <View className="flex justify-between text-18  mb-8 leading-32">
-                  <Text className="flex-1">v0.2.0 公测版</Text>
-                  <Text className="text-16 text-gray-400">2022.02.14</Text>
-                </View>
-                <Text className="pb-8 text-16">1. 提交审核版本</Text>
-              </View>
+              {
+                aboutList &&
+                aboutList.map((e,i) => {
+                  return <LogCard version={e.version} date={e.date} content={e.content} />
+                })
+              }
             </View>
             :
             <View className="flex flex-col leading-28 mt-7 border-0 border-solid border-t-1 border-b-1 border-gray-200 bg-white flex-nowrap box-border pt-10 px-22 pb-15 w-full">
               <Text className="text-18 text-gray-600 mb-8">简介</Text>
               <View className="flex flex-col border-0 border-l-4 pl-7 mb-8 text-14 text-gray-500 border-solid border-gray-300">
                 <Text>e广科</Text>
-                <Text>Github：https://github.com/GK-GNU-Linux/gdust-linux-wechat-app</Text>
+                <Text>Github：https://github.com/GK-GNU-Linux/new-gdust-linux-wechat-app</Text>
               </View>
-              <Text className="pb-7 text-15 text-gray-600">e广科，前端源码fork自广科小喵微信小程序，修改并适配e广科后端程序，由e广科团队开发并运营的微信小程序。</Text>
+              <Text className="pb-7 text-15 text-gray-600">e广科，目前部分样式及灵感源自广科小喵微信小程序，使用全新技术栈重新打造并适配e广科后端程序，由e广科团队开发并运营的微信小程序。</Text>
               <Text className="pb-7 text-15 text-gray-600">提供多元化的校园功能，有别于公众号的一种全新的连接用户与服务的方式，无需下载与安装即可在微信内被便捷地获取和传播，同时具有出色的使用体验。</Text>
               <Text className="text-18 text-gray-600 mb-8">免责声明</Text>
               <View className="flex flex-col border-0 border-l-4 pl-7 mb-8 text-14 text-gray-500 border-solid border-gray-300">
